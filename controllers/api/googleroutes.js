d5c73b2498fd40778506828f4a8dfbc1b5256ee1
sehrishkhan336 <44490311+sehrishkhan336@users.noreply.github.com>
2023-05-31 01:25:42 -0700

    adding the Google - API

--- controllers/api/googleroutes.js
+++ controllers/api/googleroutes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
     const { zipCode, location, radius } = req.body;
 
     const geolocation = await User.create(req.body);
-    const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipCode},${location},${radius}&key=YOUR_GEOCODING_API_KEY`;
+    const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipCode},${location}&key=AIzaSyCppntGd7uA7jU_xH_ocsTMXk4oXh_fIZI`;
     const parameters = req.body.param1;
 
     axios.get(geocodingUrl + parameters)
@@ -34,33 +34,43 @@ router.get('/', async (req, res) => {
             name: place.name,
             address: place.vicinity,
             rating: place.rating,
-            // Add any other desired restaurant properties
           };
         });
-        // Make request to Pics API
-        const searchQuery = 'your_search_query';
-        const picsUrl = `https://picsapi.com/api?q=${searchQuery}&key=YOUR_PICS_API_KEY`;
-        return axios.get(picsUrl);
-      })
-      .then(picsResponse => {
-        // Pics API response
-        const { data: picsData } = picsResponse;
-        // Extracting from pics response
-        const extractedData = picsData.map(item => {
-          return {
-            // deciding on which properties of the pics-api to use
-            property1: item.prop1,
-            property2: item.prop2,
+        console.log(JSON.stringify(placesResponse.data));
 
-          };
-        });
-        // session save and response
+
+        // Google PLace Photo API
+        const photoUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photoReference}&key=YOUR_PLACES_API_KEY`;
+        return axios.get(photoUrl);
+      })
+      .then(photoResponse => {
+        // Handle Place Photos API response
+        const { headers } = photoResponse;
+        const contentType = headers['content-type'];
+        const imageData = photoResponse.data;
+
+        // Extract necessary data from photo response
+        const photoReference = placesResults[0].photos[0].photo_reference;
+        const photoHeight = placesResults[0].photos[0].height;
+        const photoWidth = placesResults[0].photos[0].width;
+        const photoAttributions = placesResults[0].photos[0].html_attributions;
+
+        // Continue with the session save and response
         req.session.save(() => {
           req.session.user_id = geolocation.id;
           req.session.logged_in = true;
-
-          res.status(200).json({ geolocation, nearbyRestaurants, extractedData });
         });
+        // including extracted data for  photo image in response.
+        const extractedData = {
+          contentType,
+          imageData,
+          photoReference,
+          photoHeight,
+          photoWidth,
+          photoAttributions
+        };
+
+        res.status(200).json({ geolocation, nearbyRestaurants, extractedData });
       })
       .catch(error => {
         res.status(500).json({ error: error.message });
