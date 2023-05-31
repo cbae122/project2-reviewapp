784205daed6b6efddf31ecfe6f3b6a303062389e
hokin2023 <hokin990@gmail.com>
2023-05-30 20:42:13 -0700

    added models and seeds

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const commentRoutes = require('./commentRoutes');
+
 
 router.use('/users', userRoutes);
-router.use('/reviews', )
+router.use('/comments', commentRoutes);
+
 module.exports = router;
