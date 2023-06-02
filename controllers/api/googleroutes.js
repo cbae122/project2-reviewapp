28fec1cd8416a165d3405c9601efaef7f6bf1326
sehrishkhan336 <44490311+sehrishkhan336@users.noreply.github.com>
2023-06-01 21:04:34 -0700

    updating the new google photo routes

--- controllers/api/googleroutes.js
+++ controllers/api/googleroutes.js
@@ -5,26 +5,26 @@ const axios = require('axios');
 router.get('/:zip/:location?', async (req, res) => {
   try {
     // const { zipCode, location, radius } = req.body;
-
     // const geolocation = await User.create(req.body);
     // https://maps.googleapis.com/maps/api/geocode/json?address="91765"&key=
-    const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.params.zip},${location}&key=${process.env.API_KEY}`;
+
+    const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.params.zip}&key=${process.env.API_KEY}`;
     // const parameters = req.body.param1;
 
     return axios.get(geocodingUrl)
       .then(geocodingResponse => {
-        console.log(geocodingResponse.data);
+        // console.log(geocodingResponse.data);
         // Geocoding API response
         const { results } = geocodingResponse.data;
+        console.log(results);
         if (results.length === 0) {
           throw new Error('Invalid zip code');
         }
         // Extracting data from geocoding response
         const { lat, lng } = results[0].geometry.location;
-
         // Make request to Places API
-        const placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&type=restaurant&key=${process.env.API_KEY}`;
-        // tested the api in insominia 
+        const placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=8500&type=restaurant&key=${process.env.API_KEY}`;
+        // tested the api in insominia
         // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=34.0286,-117.8103&radius=100&type=restaurant&key=
         return axios.get(placesUrl);
       })
@@ -32,51 +32,48 @@ router.get('/:zip/:location?', async (req, res) => {
         //Places API response
         const { results } = placesResponse.data;
         //Extracting data from places response
-        const nearbyRestaurants = placesResults.map(place => {
+        const nearbyRestaurants = results.map(place => {
           return {
             name: place.name,
             address: place.vicinity,
             rating: place.rating,
             photoReference: place.photos ? place.photos[0].photo_reference : null,
             openingHours: place.opening_hours ? place.opening_hours.weekday_text : null,
-          
           };
         });
         // console.log(JSON.stringify(placesResponse.data));
-        
+        return nearbyRestaurants;
         // const placeDetailsPromises = placesResults.map(place => {
         //   const placeDetailsUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place.place_id}&key=${process.env.API_KEY}`;
         //   return axios.get(placeDetailsUrl);
         // });
-
         // return Promise.all(placeDetailsPromises);
       })
       .then(placeDetailsResponses => {
         console.log(placeDetailsResponses);
-        // // const extractedData = placeDetailsResponses.map(response => {
-        // //   const { result } = response.data;
-
-        // //   return {
-        // //     address: result.formatted_address,
-        // //     location: result.geometry.location,
-        // //     photoReference: result.photos ? result.photos[0].photo_reference : null,
-        // //     openingHours: result.opening_hours ? result.opening_hours.weekday_text : null,
-        // //     // reviews: result.reviews ? result.reviews.map(review => ({
-        // //     //   author: review.author_name,
-        // //     //   rating: review.rating,
-        // //     //   text: review.text,
-        // //     // })) : null,
+        // const extractedData = placeDetailsResponses.map(response => {
+        //   const { result } = response.data;
+        //   return {
+        //     address: result.formatted_address,
+        //     location: result.geometry.location,
+        //     photoReference: result.photos ? result.photos[0].photo_reference : null,
+        //     openingHours: result.opening_hours ? result.opening_hours.weekday_text : null,
+        //     // reviews: result.reviews ? result.reviews.map(review => ({
+        //     //   author: review.author_name,
+        //     //   rating: review.rating,
+        //     //   text: review.text,
+        //     // })) : null,
         //   };
         // });
-
+        
         res.status(200).json({ placeDetailsResponses });
       })
       .catch(error => {
         res.status(500).json({ error: error.message });
       });
   } catch (err) {
-    res.status(400).json(err);
-  } 
+    res.status(400).json(err.message);
+  }
 });
 
 module.exports = router;
--- public/js/search.js
+++ public/js/search.js
@@ -1,44 +1,82 @@
+// checks if the browser supports the Geolocation API
 if (!navigator.geolocation) {
   console.error('Your browser doesn\'t support Geolocation');
 } else {
-  console.log('OK!');A;
+  console.log('OK!');
 }
-  
 navigator.geolocation.getCurrentPosition(success, error);
-  
 function success(position) {
   const { latitude, longitude } = position.coords;
-  
   console.log(`lat: ${latitude} lon ${longitude}`);
 }
-  
 function error() {
   console.log('something failed');
 }
-
-const searchButton = document.querySelector('search-btn');
-
-
-
-searchButton.addEventListener('click', function(){
-  var position = document.getElementById('food-search').value;
-
-  fetch('/api/google');
-
-  // var axios = require('axios');
-
-  // var config = {
-  //   method: 'get',
-  //   url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position}&radius=1500&type=restaurant&keyword=cruise&key=${MAPS_API_KEY}'`
-  //   headers: { }
-  // };
-    
-  // axios(config)
+var searchButton = document.querySelector('.btn');
+// var repoList = document.querySelector('ul');
+searchButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  //   var name = document.getElementById('food-search').value;
+  // var address =
+  // fetch('/api/google')
   // .then(function (response) {
-  //   console.log(JSON.stringify(response.data));
+  //   return response.json();
   // })
-  // .catch(function (error) {
-  //   console.log(error);
+  // .then(function (data) {
+  //   // console.log(final);
+  //   for (var i = 0; i < 4 ; i++) {
+  //     var listItem = document.createElement('li');
+  //     listItem.textContent = data[i].name;
+  //     listItem.textContent = data[i].address;
+  //     listItem.textContent = data[i].rating;
+  //     repoList.appendChild(listItem);
+  //   }
+  //   final.push(data);()
+  var resResult = document.getElementById('res-results');
+  var zipcode = document.querySelector('.form-input').value;
   // });
-});
-
+  fetch(`/api/google/${zipcode}`)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert('Failed to retrieve data from Last.fm API.');
+      }
+    })
+    // .catch(function (error) {
+    //     Alert('An error occurred while fetching data from Last.fm API.');
+    // });
+    // placeDetailsResponses.forEach(function (data) {})
+    // catch (function (err) {
+    //   console.log(err);
+    // })
+    .then(function ({placeDetailsResponses}) {
+      console.log(placeDetailsResponses);
+      var html = '<h3>Here are the top 8 places you may like!</h3>';
+      if (placeDetailsResponses.length > 0) {
+        html += '';
+        for (var i = 0; i < 8; i++) {
+          var name = placeDetailsResponses[i].name;
+          var address = placeDetailsResponses[i].address;
+          var rating = placeDetailsResponses[i].rating;
+          var img = placeDetailsResponses[i].photoReference;
+          html +=
+          ` <li>
+          <img src=${img}>
+          <div>
+          <h1>${name}<h1>
+          <p>Address: ${address}<p>
+           <p>Rating: ${rating}<p>
+           <input placeholder='Let us know your thought about this place!'></input>
+           <button type=submit class="profile">Add</button>
+           </div>
+           </li>
+         `;
+          resResult.innerHTML = html;
+        }
+      }
+    }
+    );
+}
+  
+);
