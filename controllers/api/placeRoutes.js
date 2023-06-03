6c4f8c4d6a0fdcaa951a0dde6d17acaec130f2d6
hokin2023 <hokin990@gmail.com>
2023-06-02 18:03:13 -0700

    added profile routes and comment routes

--- controllers/api/placeRoutes.js
+++ controllers/api/placeRoutes.js
@@ -11,22 +11,23 @@ router.get('/', async (req, res) => {
                 'name',
                 'address',
                 'rating',
+                // 'img',
                 'created_at'
 
             ],
-            order: [['created_at', 'DESC']],
-            include: [
-                // Comment model here -- attached username to comment
-                {
-                    model: Comment,
-                    attributes: ['id', 'comment_text', 'place_id', 'user_id', 'created_at'],
+            // order: [['created_at', 'DESC']],
+            // include: [
+            //     // Comment model here -- attached username to comment
+            //     {
+            //         model: Comment,
+            //         attributes: ['id', 'comment_text', 'place_id', 'user_id', 'created_at'],
                     include: {
                         model: User,
                         attributes: ['username']
                     }
-                },
+            //     },
                 
-            ]
+            // ]
         })
         res.status(200).json(placeData);
     } catch (err) {
@@ -36,12 +37,15 @@ router.get('/', async (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     try {
-        const placeData = Place.create({
+        const placeData = await Place.create({
             name: req.body.name,
             address: req.body.address,
-            rating: req.body.rating
+            rating: req.body.rating,
+            img: req.body.img,
+            user_id: req.session.user_id
+            // img: req.body.rating
         });
         res.status(200).json(placeData);
     } catch (err) {
@@ -49,5 +53,65 @@ router.post('/', (req, res) => {
     }
 
 });
+
+router.get('/:id', (req, res) => {
+    Place.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'name',
+        'created_at',
+        'address',
+        'rating'
+      ],
+      include: [
+        // include the Comment model here:
+        {
+          model: User,
+          attributes: ['username']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'place_id', 'user_id', 'created_at'],
+        //   include: {
+        //     model: User,
+        //     attributes: ['username']
+        //   }
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.delete('/:id', withAuth, (req, res) => {
+    Place.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No place found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;    
