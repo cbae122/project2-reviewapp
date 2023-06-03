09384a632ffc6262d0111f33f800f3edfab50072
hokin2023 <125165615+hokin2023@users.noreply.github.com>
2023-06-02 18:10:36 -0700

    Update review.js

--- public/js/review.js
+++ public/js/review.js
@@ -1,27 +1,27 @@
-const newFormHandler = async (event) => {
+// const newFormHandler = async (event) => {
 
-  event.preventDefault();
+//   event.preventDefault();
   
-  const name = document.querySelector('#restaurant-name').value.trim();
-  const review = document.querySelector('#restaurant-review').value.trim();
+//   const name = document.querySelector('#restaurant-name').value.trim();
+//   const review = document.querySelector('#restaurant-review').value.trim();
   
-  if (name && review) {
-    const response = await fetch('/api/projects', {
-      method: 'POST',
-      body: JSON.stringify({ name, review }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+//   if (name && review) {
+//     const response = await fetch('/api/projects', {
+//       method: 'POST',
+//       body: JSON.stringify({ name, review }),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
   
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to create project');
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace('/profile');
+//     } else {
+//       alert('Failed to create project');
+//     }
+//   }
+// };
   
-document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
+// document
+//   .querySelector('.new-project-form')
+//   .addEventListener('submit', newFormHandler);
