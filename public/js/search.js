939d5232e92f90be198dc198e8e6346e8afaccda
sehrishkhan336 <44490311+sehrishkhan336@users.noreply.github.com>
2023-06-06 23:09:17 -0700

    cleaning the file

--- public/js/search.js
+++ public/js/search.js
@@ -71,47 +71,20 @@ searchButton.addEventListener('click', async function (event) {
 });
  
 
-// if (!navigator.geolocation) {
-//   console.error('Your browser doesn\'t support Geolocation');
-// } else {
-//   console.log('OK!');A;
-// }
+if (!navigator.geolocation) {
+  console.error('Your browser doesn\'t support Geolocation');
+} else {
+  console.log('OK!');A;
+}
   
-// navigator.geolocation.getCurrentPosition(success, error);
+navigator.geolocation.getCurrentPosition(success, error);
   
-// function success(position) {
-//   const { latitude, longitude } = position.coords;
+function success(position) {
+  const { latitude, longitude } = position.coords;
   
-//   console.log(`lat: ${latitude} lon ${longitude}`);
-// }
+  console.log(`lat: ${latitude} lon ${longitude}`);
+}
   
-// function error() {
-//   console.log('something failed');
-// }
-
-// const searchButton = document.querySelector('search-btn');
-
-
-
-// searchButton.addEventListener('click', function(){
-//   var position = document.getElementById('food-search').value;
-
-//   fetch('/api/google')
-
-// var axios = require('axios');
-
-// var config = {
-//   method: 'get',
-//   url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position}&radius=1500&type=restaurant&keyword=cruise&key=${MAPS_API_KEY}'`
-//   headers: { }
-// };
-    
-// axios(config)
-// .then(function (response) {
-//   console.log(JSON.stringify(response.data));
-// })
-// .catch(function (error) {
-//   console.log(error);
-// });
-// });
-
+function error() {
+  console.log('something failed');
+}
